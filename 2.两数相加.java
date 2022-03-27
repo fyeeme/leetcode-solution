086f4b4255a5d111243d6ef5b011686d3f136712
LiuYang <fyeeme@gmail.com>
2022-03-27 21:43:29 +0800

    feat: add solution

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -8,17 +8,53 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        ListNode l0 = new ListNode(0);
+        // 移动指针, 移动完毕后, curr 会指向最后一个节点
+        ListNode curr = l0;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+        // 返回从下标1开始的链表
+        return l0.next;
     }
+
+    // class ListNode {
+    // int val;
+    // ListNode next;
+
+    // ListNode() {
+    // }
+
+    // ListNode(int val) {
+    // this.val = val;
+    // }
+
+    // ListNode(int val, ListNode next) {
+    // this.val = val;
+    // this.next = next;
+    // }
+    // }
 }
 // @lc code=end
-
